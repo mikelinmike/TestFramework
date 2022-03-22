8c80bf6f553901792cdbcf2f635e72a1e57edb4c
mikelin <mikelin@wondercore.com>
2022-03-22 14:14:08 +0800

    delete some file, add one class

---
+++ TestFramework/TestFramework/ShowString.swift
@@ -0,0 +1,16 @@
+//
+//  ShowString.swift
+//  TestFramework
+//
+//  Created by wondercise on 2022/3/22.
+//
+
+import Foundation
+
+public class ShowString{
+    public init(){}
+    
+    public func showString(str: String){
+        print("str => \(str)")
+    }
+}
